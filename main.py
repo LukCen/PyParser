2b1672fcb743c5db3389769d3afd132f71594dc3
lbjcena <lbjcena@gmail.com>
2026-06-18 01:44:01 +0200

    reduced amount of rows shown, pagination added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utils.file_parser import FileParser
 app = Flask(__name__)
 new_file = FileParser("./data.csv")
@@ -6,4 +6,11 @@ new_file = FileParser("./data.csv")
 @app.route("/")
 def landing():
   parsed = new_file.read_csv()
-  return render_template("index.html", columns=parsed.columns.tolist(), rows=parsed.values.tolist())
+  page = request.args.get('page', 1, type=int)
+  per_page = 20
+  start = (page-1) * per_page
+  end = start + per_page
+  return render_template("index.html", 
+                         columns=parsed.columns.tolist(), 
+                         rows=parsed.values.tolist()[start:end], 
+                         current_page=page)
