acb1ade002f1aa87c05b415a4e8a98d150c48a99
lbjcena <lbjcena@gmail.com>
2026-06-19 18:44:56 +0200

    file upload added and wired

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request
 from utils.file_parser import FileParser
 app = Flask(__name__)
-new_file = FileParser("./data.csv")
 
-@app.route("/")
+def filtered_data(data_from_file, type, index):
+  if type=="column":
+    return data_from_file[index]
+
+@app.route("/", methods=["GET","POST"])
 def landing():
-  parsed = new_file.read_csv()
+  new_file = request.files.get('user_file')
   page = request.args.get('page', 1, type=int)
   per_page = 20
   start = (page-1) * per_page
   end = start + per_page
-  return render_template("index.html", 
-                         columns=parsed.columns.tolist(), 
-                         rows=parsed.values.tolist()[start:end], 
-                         current_page=page)
+  
+  if new_file is not None:
+    parsed = FileParser(new_file).read_csv()
+    return render_template("index.html", 
+                          columns=parsed.columns.tolist(), 
+                          rows=parsed.values.tolist()[start:end], 
+                          current_page=page)
+  else: return render_template("index.html", columns=None, rows=None, current_page=page)
